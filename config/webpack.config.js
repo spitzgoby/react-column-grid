94b0b001b0777cb1907038e887d571b2e26b844d
Tom Leu <tomleu@gmail.com>
2021-11-02 22:08:56 -0500

    initialize project template

---
+++ config/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        static: {
+            directory: path.resolve('dist')
+        }
+    },
+    devtool: 'source-map',
+    entry: {
+        main: path.resolve('src/index.js'), 
+        example: path.resolve('example/index.js')
+    },
+    mode: process.env.NODE_ENV || 'development',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: { modules: [path.resolve(__dirname, 'src'), 'node_modules'] },
+    output: {
+        clean: true,
+        filename: '[name].js',
+        path: path.resolve('dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve('example/index.html')
+        })
+    ]
+};
---
+++ example/App.js
@@ -0,0 +1,10 @@
+import './App.scss';
+import React from 'react';
+
+export default props => {
+    return (
+        <div className='App'>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
